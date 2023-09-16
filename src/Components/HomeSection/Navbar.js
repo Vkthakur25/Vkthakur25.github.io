7e2eeaeed5147ae253c6ac47e8ca96086f14ca0f
Vkthakur25 <vivek.kumarr166@gmail.com>
2023-09-16 12:54:46 +0530

    2 files modified

--- src/Components/HomeSection/Navbar.js
+++ src/Components/HomeSection/Navbar.js
@@ -12,9 +12,12 @@ function Navbar() {
         </div>
         <div
           className=""
-          style={{ height: '80vh', width: '10vw', border: '2px solid black' }}
-        >
-          hii i am Border vertical
+          style={{ height: '80vh', width: '10vw', border: '2px solid black' }}>
+          <div className="social-box ">
+            <img className="social" src="https://d33wubrfki0l68.cloudfront.net/d8e6e1e636531e28274a1b8b6d947b817f6145bd/d42d3/assets/svg/linkedin-dark.svg" alt="" />
+            <img className="social" src="https://d33wubrfki0l68.cloudfront.net/38469cf88d038b6ba3322c9fcb93a8f7167df4b9/cb0b9/assets/svg/github-dark.svg" alt="" />
+            <img className="social" src="https://www.svgrepo.com/show/424905/instagram-logo-facebook.svg" alt="" />
+          </div>
         </div>
       </section>
     </>
