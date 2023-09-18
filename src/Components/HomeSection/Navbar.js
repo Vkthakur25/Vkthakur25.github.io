cdb2f22605d84e0aa22011f695cf4bd2527d3e14
Vkthakur25 <vivek.kumarr166@gmail.com>
2023-09-18 14:38:06 +0530

    2 files modified

--- src/Components/HomeSection/Navbar.js
+++ src/Components/HomeSection/Navbar.js
@@ -8,15 +8,53 @@ function Navbar() {
           className=""
           style={{ height: '12vh', width: '94vw', border: '2px solid black' }}
         >
-          hii i am Border horizontal
+          <ul className="d-flex" style={{ 'margin-left': '7rem' }}>
+            <li className=" fs-4 list-unstyled">
+              <a className="text-decoration-none text-black" href="">
+                {' '}
+                Home
+              </a>
+            </li>
+            <li className="mx-4 fs-4 list-unstyled">
+              <a className="text-decoration-none text-black" href="">
+                {' '}
+                About
+              </a>
+            </li>
+            <li className="mx-4 fs-4 list-unstyled">
+              <a className="text-decoration-none text-black" href="">
+                {' '}
+                Projects
+              </a>
+            </li>
+            <li className="mx-4 fs-4 list-unstyled">
+              <a className="text-decoration-none text-black" href="">
+                {' '}
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
         <div
           className=""
-          style={{ height: '80vh', width: '6vw', border: '2px solid black' }}>
+          style={{ height: '80vh', width: '6vw', border: '2px solid black' }}
+        >
           <div className="social-box ">
-            <img className="social" src="https://d33wubrfki0l68.cloudfront.net/d8e6e1e636531e28274a1b8b6d947b817f6145bd/d42d3/assets/svg/linkedin-dark.svg" alt="" />
-            <img className="social" src="https://d33wubrfki0l68.cloudfront.net/38469cf88d038b6ba3322c9fcb93a8f7167df4b9/cb0b9/assets/svg/github-dark.svg" alt="" />
-            <img className="social" src="https://www.svgrepo.com/show/424905/instagram-logo-facebook.svg" alt="" />
+            <img
+              className="social"
+              src="https://d33wubrfki0l68.cloudfront.net/d8e6e1e636531e28274a1b8b6d947b817f6145bd/d42d3/assets/svg/linkedin-dark.svg"
+              alt=""
+            />
+            <img
+              className="social"
+              src="https://d33wubrfki0l68.cloudfront.net/38469cf88d038b6ba3322c9fcb93a8f7167df4b9/cb0b9/assets/svg/github-dark.svg"
+              alt=""
+            />
+            <img
+              className="social"
+              src="https://www.svgrepo.com/show/424905/instagram-logo-facebook.svg"
+              alt=""
+            />
           </div>
         </div>
       </section>
