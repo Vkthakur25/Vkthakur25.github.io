bb8e9f02fddaf9c9b8262216e791de3a46fea89b
Vkthakur25 <vivek.kumarr166@gmail.com>
2023-09-18 17:31:29 +0530

    2 files modified

--- src/Components/HomeSection/Navbar.js
+++ src/Components/HomeSection/Navbar.js
@@ -37,21 +37,41 @@ function Navbar() {
         </div>
         <div className="" style={{ height: '80vh', width: '6vw' }}>
           <div className="social-box ">
-            <img
-              className="social"
-              src="https://d33wubrfki0l68.cloudfront.net/d8e6e1e636531e28274a1b8b6d947b817f6145bd/d42d3/assets/svg/linkedin-dark.svg"
-              alt=""
-            />
-            <img
-              className="social"
-              src="https://d33wubrfki0l68.cloudfront.net/38469cf88d038b6ba3322c9fcb93a8f7167df4b9/cb0b9/assets/svg/github-dark.svg"
-              alt=""
-            />
-            <img
-              className="social"
-              src="https://www.svgrepo.com/show/424905/instagram-logo-facebook.svg"
-              alt=""
-            />
+            <a href="">
+              <img
+                className="social"
+                src="https://d33wubrfki0l68.cloudfront.net/d8e6e1e636531e28274a1b8b6d947b817f6145bd/d42d3/assets/svg/linkedin-dark.svg"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className="social"
+                src="https://d33wubrfki0l68.cloudfront.net/38469cf88d038b6ba3322c9fcb93a8f7167df4b9/cb0b9/assets/svg/github-dark.svg"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className="social"
+                src="https://www.svgrepo.com/show/424905/instagram-logo-facebook.svg"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className="social"
+                src="https://www.svgrepo.com/show/306328/leetcode.svg"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className="social"
+                src="https://www.svgrepo.com/show/364439/discord-logo-fill.svg"
+                alt=""
+              />
+            </a>
           </div>
         </div>
       </section>
