c7b3850e2f61f27310103166ec0d0cf17710e0ad
Vkthakur25 <vivek.kumarr166@gmail.com>
2023-09-25 14:49:27 +0530

    1 file modified

--- src/Components/HomeSection/Navbar.js
+++ src/Components/HomeSection/Navbar.js
@@ -8,25 +8,21 @@ function Navbar() {
           <ul className="d-flex mx-5 p-4">
             <li className=" fs-4 list-unstyled">
               <a className="text-decoration-none text-black" href="">
-                {' '}
                 Home
               </a>
             </li>
             <li className="mx-5 fs-4 list-unstyled">
               <a className="text-decoration-none text-black" href="">
-                {' '}
                 About
               </a>
             </li>
             <li className="mx-4 fs-4 list-unstyled">
               <a className="text-decoration-none text-black" href="">
-                {' '}
                 Projects
               </a>
             </li>
             <li className="mx-5 fs-4 list-unstyled">
               <a className="text-decoration-none text-black" href="">
-                {' '}
                 Contact
               </a>
             </li>
